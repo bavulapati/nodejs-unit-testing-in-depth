6ebe7ea29a0b357fe15a24d7b74de1c7cc2c4055
Balakrishna Avulapti <bavulapti@gmail.com>
2020-02-06 00:16:18 +0530

    befor/after

--- test/test.js
+++ test/test.js
@@ -2,6 +2,14 @@ const assert = require('assert')
 
 describe('file to be tested', ()=>{
 	context('function to be tested', ()=>{
+		before(()=>{
+			console.log('=======before')
+		})
+
+		after(()=>{
+			console.log('=======after')
+		})
+		
 		it('should do something', ()=>{
 			assert.equal(1,1)
 		})
